4bfeb45051d3678e08a88c021b2cf5eafa8ea317
Yamin Himani <yaminhimani@outlook.com>
2020-05-11 19:52:30 -0400

    initial commit

---
+++ ruby.rb
@@ -0,0 +1,7 @@
+users = [
+          { username: "Yamin", password: "password1" },
+          { username: "Delsin", password: "password2" },
+          { username: "Mannny", password: "password3" },
+          { username: "Angus", password: "password4" },
+          { username: "Steven", password: "password5" }
+        ]
